951a96a6b520c57a111d4e6eaf52d7b6977baa07
torikoc <breakthroughtgw@gmail.com>
2019-04-17 21:48:02 +0800

    replace next button with forget/remember button

--- src/components/Room/index.vue
+++ src/components/Room/index.vue
@@ -15,7 +15,10 @@
       </v-layout>
     </v-container>
     <div class="actions">
-      <v-btn v-if="hasAnswer" color="primary" block @click="next">Next</v-btn>
+      <div v-if="hasAnswer" class="actions__buttons">
+        <v-btn color="yellow" block @click.prevent="forget">forget</v-btn>
+        <v-btn color="primary" block @click.prevent="remember">remember</v-btn>
+      </div>
       <v-btn v-else block @click="showAnswer" color="success">show answer</v-btn>
     </div>
     <v-snackbar :top="true" :timeout="3000" v-model="snackbar">没有更多卡片了</v-snackbar>
@@ -57,6 +60,24 @@ export default {
     showAnswer() {
       document.querySelector(".back").style.display = "block";
       this.hasAnswer = true;
+    },
+    forget() {
+      let body = Object.assign({}, this.cards[this.idx]);
+      if (body.counter) {
+        body.counter.appear += 1;
+        body.counter.forget += 1;
+        api.updateCard(body._id, body);
+      }
+      this.next();
+    },
+    remember() {
+      let body = Object.assign({}, this.cards[this.idx]);
+      if (body.counter) {
+        body.counter.appear += 1;
+        body.counter.remember += 1;
+        api.updateCard(body._id, body);
+      }
+      this.next();
     },
     next() {
       if (this.idx + 1 >= this.cards.length) {
@@ -110,6 +131,10 @@ export default {
   width: 100%;
   position: fixed;
   bottom: 0;
+}
+.actions__buttons {
+  display: flex;
+  flex-direction: row;
 }
 .actions button {
   margin: 0;
